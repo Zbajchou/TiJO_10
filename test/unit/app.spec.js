2c27d58aceca159599cbfc4f8f3671dc055fdcfc
littlejohn <dariusz.piwko@gmail.com>
2016-05-11 12:14:35 +0200

    nice tests

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -5,8 +5,8 @@ describe('app', function () {
 
     describe('assignToSwimmingCourse', function () {
         it('should assign adult person to adult group', function () {
-            expect(app.assignToSwimmingCourse('Jack', '10/10/1995')).toEqual({
-                name: 'Jack', age: 20, course: 'adults'
+            expect(app.assignToSwimmingCourse('Zbyszek', '10/10/1995')).toEqual({
+                name: 'Zbyszek', age: 20, course: 'PHP-developer'
             });
         });
         it('should assign person between the ages of 12 - 17 to teens group', function () {
@@ -16,8 +16,30 @@ describe('app', function () {
         });
         it('should assign person below 12 to kids group', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
-                name: 'Zack', age: 5, course: 'kids'
+                name: "Zack", age: 5, course: 'kids'
             });
+        });
+        it('should calculate age', function(){
+            expect(app.calculateAge('19/02/2016')).toEqual(-2);
+        });
+        it('should calculate age', function(){
+            expect(app.calculateAge("19/03/1999")).toEqual(15);
+        });
+        it('should ', function(){
+        
+        });
+        it('should calculate Area', function(){
+            expect(app.calculateArea(1,2,3, 'You\'re the best', "buuuu")).toEqual({
+                area: -8, message: 'buuuu'
+            });
+        });
+        it('should calculate Area', function(){
+            expect(app.calculateArea(5,51,1, 'Ok', 'notOk')).toEqual({
+                area: 46, message: 'Ok'
+            });
+        });
+        it('should calculate and return value', function(){
+            expect();
         });
     });
 });
